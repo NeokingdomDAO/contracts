65416bf1ad6483f8081e84aed0ec752bc7b641f4
Nicola Miotto <miotto@posteo.de>
2022-01-19 11:46:46 +0100

    poc for contract upgrading (on TelediskoToken)

--- contracts/TelediskoToken.sol
+++ contracts/TelediskoToken.sol
@@ -4,17 +4,22 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-abstract contract TelediskoToken is ERC20Snapshot {
+abstract contract TokenLogic {
+    bool offersHalted = false;
+    
     uint256 public totalVotingTokens;
     uint256 constant OFFER_DURATION = 2 weeks;
     address constant DAI_ADDRESS = 0x1111111111111111111111111111111111111111;
+    address TT_ADDRESS = 0x1111111111111111111111111111111111111111;
+
+    IERC20 telediskoToken = IERC20(TT_ADDRESS); 
+    IERC20 dai = IERC20(DAI_ADDRESS);
 
     //mapping(address => mapping(uint256 => uint256)) offers;
     mapping(address => uint256) offered;
-
     mapping(address => Offer[]) offers;
-
     mapping(address => uint256) allowance;
 
     struct Offer {
@@ -22,28 +27,29 @@ abstract contract TelediskoToken is ERC20Snapshot {
         uint256 amount;
     }
 
+    modifier onlyContributor() {
+        require(isContributor(msg.sender), "TT: not a contributor");
+        _;
+    }
+
+    modifier onlyMaintainer() {
+        _;
+    }
+
     function isContributor(address a) internal view returns (bool) {}
 
-    modifier onlyContributor() {
-        require(isContributor(_msgSender()), "TT: not a contributor");
-        _;
+    function setHaltedOffers(bool halted) external onlyMaintainer {
+        offersHalted = halted;
     }
-
-    modifier onlyAdmin() {
-        _;
-    }
-
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
-
-    function mint(address, uint256) public onlyAdmin {}
 
     function offer(uint256 amount) public onlyContributor {
+        require(!offersHalted);
         require(
-            balanceOf(_msgSender()) - offered[_msgSender()] >= amount,
+            telediskoToken.balanceOf(msg.sender) - offered[msg.sender] >= amount,
             "TT: not enough balance"
         );
         // Create offer
-        offered[_msgSender()] += amount;
+        offered[msg.sender] += amount;
         // offers[_msgSender()][block.timestamp] = amount;
         // Add offers to various mappings
     }
@@ -54,16 +60,15 @@ abstract contract TelediskoToken is ERC20Snapshot {
         uint256 amount
     ) public payable onlyContributor {
         // Contributor needs to call `approve` on the DAI contract
-        IERC20 dai = IERC20(DAI_ADDRESS);
+        
         Offer memory o = offers[from][id];
-        dai.transferFrom(_msgSender(), from, amount);
+        dai.transferFrom(msg.sender, from, amount);
         // remove offer from array
-        _transfer(from, _msgSender(), amount);
+        telediskoToken.transferFrom(from, msg.sender, amount);
     }
 
     function updateAllowance(address contributor)
         public
-        view
         returns (uint256)
     {
         uint256 total;
@@ -78,28 +83,69 @@ abstract contract TelediskoToken is ERC20Snapshot {
         allowance[contributor] += total;
     }
 
-    function _beforeTokenTransfer(
+    function beforeTokenTransfer(
         address from,
         address,
         uint256 amount
-    ) internal view override {
-        if (isContributor(_msgSender())) {
-            updateAllowance(_msgSender());
+    ) external {
+        if (isContributor(msg.sender)) {
+            updateAllowance(msg.sender);
         }
         require(
-            !isContributor(_msgSender()) || allowance[from] <= amount,
+            !isContributor(msg.sender) || allowance[from] <= amount,
             "TelediskoToken: not enough allowance"
         );
     }
 
-    function _afterTokenTransfer(
+    function afterTokenTransfer(
         address from,
         address,
         uint256 amount
-    ) internal view override {
-        if (isContributor(_msgSender())) {
+    ) external {
+        if (isContributor(msg.sender)) {
             allowance[from] -= amount;
-            offered[_msgSender()] -= amount;
+            offered[msg.sender] -= amount;
         }
+    }
+}
+
+
+abstract contract TelediskoToken is ERC20Snapshot, AccessControl {
+    
+    bytes32 public constant TOKEN_CONTROLLER = keccak256("TOKEN_CONTROLLER");
+
+    TokenLogic tokenLogic = TokenLogic(0x1111111111111111111111111111111111111111);
+
+    modifier onlyAdmin() {
+        _;
+    }
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint(address, uint256) public onlyAdmin {}
+
+    function transferFrom(address from, address to, uint256 amount) public override returns (bool){
+        if(hasRole(TOKEN_CONTROLLER, _msgSender())) {
+            _transfer(from, to, amount);
+        }
+        else {
+            super.transferFrom(from, to, amount);
+        }
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {
+        tokenLogic.beforeTokenTransfer(from, to, amount);
+    }
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {
+        tokenLogic.afterTokenTransfer(from, to, amount);
     }
 }

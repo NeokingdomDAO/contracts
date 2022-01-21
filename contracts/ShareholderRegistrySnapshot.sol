20e568bb0a32cbb9170fb7f4e6687fb1a9835ccc
vrde <agranzot@mailbox.org>
2022-01-21 14:10:43 +0100

    Move snapshottable to extension dir

--- contracts/ShareholderRegistrySnapshot.sol
+++ contracts/ShareholderRegistrySnapshot.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 
 //import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./ShareholderRegistryBase.sol";
-import "./Snapshottable.sol";
+import "./extensions/Snapshottable.sol";
 
 contract ShareholderRegistrySnapshot is ShareholderRegistryBase, Snapshottable {
     struct TotalSupplySnapshots {

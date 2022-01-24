29fd92bee5add85bdab6f0a43f2bb8fb55a79aa5
Nicola Miotto <miotto@posteo.de>
2022-01-24 16:15:52 +0100

    increase test coverage VotingSnapshot

--- test/VotingSnapshot.ts
+++ test/VotingSnapshot.ts
@@ -31,7 +31,7 @@ describe("VotingSnapshot", () => {
     nonContributor: SignerWithAddress;
 
   beforeEach(async () => {
-    [delegator1, delegator2, delegated1, delegated2, anon, nonContributor] =
+    [delegator1, delegator2, delegated1, delegated2, nonContributor] =
       await ethers.getSigners();
     const VotingSnapshotFactory = (await ethers.getContractFactory(
       "VotingSnapshot",
@@ -70,45 +70,203 @@ describe("VotingSnapshot", () => {
     it("should increase snapshot id", async () => {
       await votingSnapshot.snapshot();
       let snapshotIdBefore = await votingSnapshot.getCurrentSnapshotId();
-
       await votingSnapshot.snapshot();
       let snapshotIdAfter = await votingSnapshot.getCurrentSnapshotId();
 
       expect(snapshotIdBefore.toNumber()).lessThan(snapshotIdAfter.toNumber());
     });
 
-    it("should return the delegate at the time of the snapshot", async () => {
-      await votingSnapshot.connect(delegator1).delegate(delegated1.address);
-      await votingSnapshot.snapshot();
-      let snapshotIdBefore = await votingSnapshot.getCurrentSnapshotId();
+    describe("getDelegateAt", async () => {
+      it("reverts with snapshot id of 0", async () => {
+        await expect(
+          votingSnapshot.getDelegateAt(anon.address, 0)
+        ).revertedWith("Snapshottable: id is 0");
+      });
 
-      await votingSnapshot.connect(delegator1).delegate(delegated2.address);
-      await votingSnapshot.snapshot();
-      let snapshotIdAfter = await votingSnapshot.getCurrentSnapshotId();
+      it("should return the delegate at the time of the snapshot", async () => {
+        await votingSnapshot.connect(delegator1).delegate(delegated1.address);
+        await votingSnapshot.snapshot();
+        let snapshotIdBefore = await votingSnapshot.getCurrentSnapshotId();
+        await votingSnapshot.connect(delegator1).delegate(delegated2.address);
+        await votingSnapshot.snapshot();
+        let snapshotIdAfter = await votingSnapshot.getCurrentSnapshotId();
 
-      expect(
-        await votingSnapshot.getDelegateAt(delegator1.address, snapshotIdBefore)
-      ).equal(delegated1.address);
-      expect(
-        await votingSnapshot.getDelegateAt(delegator1.address, snapshotIdAfter)
-      ).equal(delegated2.address);
+        expect(
+          await votingSnapshot.getDelegateAt(
+            delegator1.address,
+            snapshotIdBefore
+          )
+        ).equal(delegated1.address);
+        expect(
+          await votingSnapshot.getDelegateAt(
+            delegator1.address,
+            snapshotIdAfter
+          )
+        ).equal(delegated2.address);
+      });
+
+      it("should return the latest delegate at the time of the snapshot", async () => {
+        await votingSnapshot.connect(delegator1).delegate(delegated1.address);
+        await votingSnapshot.connect(delegator1).delegate(delegated2.address);
+        await votingSnapshot.snapshot();
+        let snapshotId = await votingSnapshot.getCurrentSnapshotId();
+
+        expect(
+          await votingSnapshot.getDelegateAt(delegator1.address, snapshotId)
+        ).equal(delegated2.address);
+      });
+
+      it("should return same delegate from different snapshots if delegation failed in between", async () => {
+        await votingSnapshot.snapshot();
+        let snapshotIdBefore = await votingSnapshot.getCurrentSnapshotId();
+        try {
+          await votingSnapshot
+            .connect(delegator1)
+            .delegate(nonContributor.address);
+        } catch {}
+        await votingSnapshot.snapshot();
+        let snapshotIdAfter = await votingSnapshot.getCurrentSnapshotId();
+
+        expect(
+          await votingSnapshot.getDelegateAt(delegator1.address, snapshotIdBefore)
+        ).equal(delegator1.address);
+
+        expect(
+          await votingSnapshot.getDelegateAt(delegator1.address, snapshotIdAfter)
+        ).equal(delegator1.address);
+      });
     });
 
-    it("should return the votes at the time of the snapshot", async () => {
-      await token.mint(delegator1.address, 10);
-      await votingSnapshot.snapshot();
-      let snapshotIdBefore = await votingSnapshot.getCurrentSnapshotId();
+    describe("getVotesAt", async () => {
+      it("reverts with snapshot id of 0", async () => {
+        await expect(votingSnapshot.getVotesAt(anon.address, 0)).revertedWith(
+          "Snapshottable: id is 0"
+        );
+      });
 
-      await token.mint(delegator1.address, 11);
-      await votingSnapshot.snapshot();
-      let snapshotIdAfter = await votingSnapshot.getCurrentSnapshotId();
+      it("should return the votes at the time of the snapshot - minting case", async () => {
+        await token.mint(delegator1.address, 10);
+        await votingSnapshot.snapshot();
+        let snapshotIdBefore = await votingSnapshot.getCurrentSnapshotId();
+        await token.mint(delegator1.address, 11);
+        await votingSnapshot.snapshot();
+        let snapshotIdAfter = await votingSnapshot.getCurrentSnapshotId();
 
-      expect(
-        await votingSnapshot.getVotesAt(delegator1.address, snapshotIdBefore)
-      ).equal(10);
-      expect(
-        await votingSnapshot.getVotesAt(delegator1.address, snapshotIdAfter)
-      ).equal(21);
+        expect(
+          await votingSnapshot.getVotesAt(delegator1.address, snapshotIdBefore)
+        ).equal(10);
+        expect(
+          await votingSnapshot.getVotesAt(delegator1.address, snapshotIdAfter)
+        ).equal(21);
+      });
+
+      it("should return the votes at the time of the snapshot - transfer case", async () => {
+        await token.mint(delegator1.address, 10);
+        await token.connect(delegator1).transfer(delegator2.address, 10);
+        await votingSnapshot.snapshot();
+        let snapshotIdBefore = await votingSnapshot.getCurrentSnapshotId();
+        await token.connect(delegator2).transfer(delegator1.address, 10);
+        await votingSnapshot.snapshot();
+        let snapshotIdAfter = await votingSnapshot.getCurrentSnapshotId();
+
+        expect(
+          await votingSnapshot.getVotesAt(delegator1.address, snapshotIdBefore)
+        ).equal(0);
+        expect(
+          await votingSnapshot.getVotesAt(delegator2.address, snapshotIdBefore)
+        ).equal(10);
+
+        expect(
+          await votingSnapshot.getVotesAt(delegator1.address, snapshotIdAfter)
+        ).equal(10);
+        expect(
+          await votingSnapshot.getVotesAt(delegator2.address, snapshotIdAfter)
+        ).equal(0);
+      });
+
+      it("should return the votes at the time of the snapshot - delegation case", async () => {
+        await token.mint(delegator1.address, 10);
+        await votingSnapshot.snapshot();
+        let snapshotIdBefore = await votingSnapshot.getCurrentSnapshotId();
+        await votingSnapshot.connect(delegator1).delegate(delegated1.address);
+        await votingSnapshot.snapshot();
+        let snapshotIdAfter = await votingSnapshot.getCurrentSnapshotId();
+
+        expect(
+          await votingSnapshot.getVotesAt(delegator1.address, snapshotIdBefore)
+        ).equal(10);
+        expect(
+          await votingSnapshot.getVotesAt(delegated1.address, snapshotIdBefore)
+        ).equal(0);
+
+        expect(
+          await votingSnapshot.getVotesAt(delegator1.address, snapshotIdAfter)
+        ).equal(0);
+        expect(
+          await votingSnapshot.getVotesAt(delegated1.address, snapshotIdAfter)
+        ).equal(10);
+      });
+
+      it("should return same votes from different snapshots if delegation failed in between", async () => {
+        await token.mint(delegator1.address, 10);
+        await votingSnapshot.snapshot();
+        let snapshotIdBefore = await votingSnapshot.getCurrentSnapshotId();
+        try {
+          await votingSnapshot
+            .connect(delegator1)
+            .delegate(nonContributor.address);
+        } catch {}
+        await votingSnapshot.snapshot();
+        let snapshotIdAfter = await votingSnapshot.getCurrentSnapshotId();
+
+        expect(
+          await votingSnapshot.getVotesAt(delegator1.address, snapshotIdBefore)
+        ).equal(10);
+        expect(
+          await votingSnapshot.getVotesAt(nonContributor.address, snapshotIdBefore)
+        ).equal(0);
+
+        expect(
+          await votingSnapshot.getVotesAt(delegator1.address, snapshotIdAfter)
+        ).equal(10);
+        expect(
+          await votingSnapshot.getVotesAt(nonContributor.address, snapshotIdAfter)
+        ).equal(0);
+      });
+
+      it.only("should return votes at the time of the snapshots when token transferred to non contributor", async () => {
+        await token.mint(delegator1.address, 10);
+        await votingSnapshot.snapshot();
+        let snapshotIdBefore = await votingSnapshot.getCurrentSnapshotId();
+        await token.connect(delegator1).transfer(nonContributor.address, 10);
+        await votingSnapshot.snapshot();
+        let snapshotIdAfter = await votingSnapshot.getCurrentSnapshotId();
+
+        expect(
+          await votingSnapshot.getVotesAt(delegator1.address, snapshotIdBefore)
+        ).equal(10);
+        expect(
+          await votingSnapshot.getVotesAt(nonContributor.address, snapshotIdBefore)
+        ).equal(0);
+
+        expect(
+          await votingSnapshot.getVotesAt(delegator1.address, snapshotIdAfter)
+        ).equal(0);
+        expect(
+          await votingSnapshot.getVotesAt(nonContributor.address, snapshotIdAfter)
+        ).equal(0);
+      });
+
+      it("should return the latest votes at the time of the snapshot", async () => {
+        await token.mint(delegator1.address, 10);
+        await token.mint(delegator1.address, 32);
+        await votingSnapshot.snapshot();
+        let snapshotId = await votingSnapshot.getCurrentSnapshotId();
+
+        expect(
+          await votingSnapshot.getVotesAt(delegator1.address, snapshotId)
+        ).equal(42);
+      });
     });
   });
 });

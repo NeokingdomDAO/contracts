d383db5981d877b925a34226a780e45b9d435d2d
vrde <agranzot@mailbox.org>
2022-03-04 15:39:09 +0100

    Update delegate task

--- tasks/delegate.ts
+++ tasks/delegate.ts
@@ -1,25 +1,13 @@
 import { task } from "hardhat/config";
-import { readFile } from "fs/promises";
 import { Voting__factory } from "../typechain";
+import { loadContract } from "./config";
 
 task("delegate", "Delegate an address")
   .addPositionalParam("delegate", "The delegate")
   .setAction(async ({ delegate }, hre) => {
-    const network = JSON.parse(
-      await readFile("./deployments/networks.json", "utf8")
-    );
-
-    const [deployer] = await hre.ethers.getSigners();
-
-    const { chainId } = await hre.ethers.provider.getNetwork();
-
-    const voting = Voting__factory.connect(
-      network[chainId]["Voting"],
-      deployer
-    );
-
-    const tx = await voting.delegate(delegate);
-    console.log("Submitted tx", tx.hash);
+    const contract = await loadContract(hre, Voting__factory, "Voting");
+    const tx = await contract.delegate(delegate);
+    console.log("  Submitted tx", tx.hash);
     const receipt = await tx.wait();
-    console.log("Transaction included in block", receipt.blockNumber);
+    console.log("  Transaction included in block", receipt.blockNumber);
   });

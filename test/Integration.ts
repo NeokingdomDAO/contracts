511b3ed118e59328d1fec8ab618abe6ad88fb61c
Nicola Miotto <miotto@posteo.de>
2022-03-16 17:41:48 +0100

    Add unhappy paths integration tests for Voting (#48)
    Test (hopefully) all cases where a vote is supposed to fail.
    
    Close #24

--- test/Integration.ts
+++ test/Integration.ts
@@ -241,7 +241,7 @@ describe("Resolution", () => {
     // Create and approve new resolution
     // Contributor with not sufficient voting power vote yes to resolution
     // Resolution fails
-    it("multiple resolutions, different voting power over time, multiple contributor", async () => {
+    it("multiple resolutions, different voting power over time, multiple contributors", async () => {
       await _prepareForVoting(user1, 66);
       await _prepareForVoting(user2, 34);
       const resolutionId1 = await _prepareResolution();
@@ -318,6 +318,60 @@ describe("Resolution", () => {
       expect(resolution2User2.isYes).false;
       expect(resolution2User2.votingPower).equal(0);
       expect(resolution2User2.hasVoted).false;
+    });
+
+    it("invalid voting should not be counted", async () => {
+      const resolutionId = ++currentResolution;
+      await _prepareForVoting(user1, 42);
+      await resolution.connect(user1).createResolution("Qxtest", 0, false);
+      // votes given before approval
+      await expect(_vote(user1, true, resolutionId)).reverted;
+
+      await resolution.connect(founder).approveResolution(currentResolution);
+      // votes given during notice
+      await expect(_vote(user1, true, resolutionId)).reverted;
+
+      await _makeVotable(resolutionId);
+
+      // votes given from non DAO members
+      await expect(_vote(user2, true, resolutionId)).reverted;
+      // votes given from less than Contributors
+      await shareholderRegistry.mint(user2.address, 1);
+      await shareholderRegistry.setStatus(investorStatus, user2.address);
+
+      const resolutionId2 = await _prepareResolution();
+      await _makeVotable(resolutionId2);
+      await expect(_vote(user2, true, resolutionId)).reverted;
+
+      // votes given after burning share
+      _prepareForVoting(user3, 42);
+      await shareholderRegistry.grantRole(
+        await roles.RESOLUTION_ROLE(),
+        deployer.address
+      );
+      await shareholderRegistry.burn(user3.address, 1);
+      const resolutionId3 = await _prepareResolution();
+      await _makeVotable(resolutionId3);
+      await expect(_vote(user3, true, resolutionId3)).reverted;
+
+      await _endResolution();
+      // votes given after closure
+      await expect(_vote(user1, true, resolutionId)).reverted;
+      const resolution1Result = await resolution.getResolutionResult(
+        resolutionId
+      );
+
+      const resolution2Result = await resolution.getResolutionResult(
+        resolutionId2
+      );
+
+      const resolution3Result = await resolution.getResolutionResult(
+        resolutionId3
+      );
+
+      expect(resolution1Result).false;
+      expect(resolution2Result).false;
+      expect(resolution3Result).false;
     });
 
     it("expect chaos", async () => {

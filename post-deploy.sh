9e8eb11a4ab2549011de7264622f05385ced1465
Nicola Miotto <miotto@posteo.de>
2022-05-27 14:19:53 +0200

    add network parameter on post-deploy script

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,11 +1,12 @@
 WALLETS_FILE=$1
+NETWORK=$1
 echo "Granting OPERATOR_ROLE on ResolutionManager"
-cat $WALLETS_FILE | xargs -n1 -IWALLET npx hardhat grant-role --contract ResolutionManager --role OPERATOR_ROLE --account WALLET --network mumbai
+cat $WALLETS_FILE | xargs -n1 -IWALLET npx hardhat grant-role --contract ResolutionManager --role OPERATOR_ROLE --account WALLET --network $NETWORK
 echo "Granting OPERATOR_ROLE on ShareholderRegistry"
-cat $WALLETS_FILE | xargs -n1 -IWALLET npx hardhat grant-role --contract ShareholderRegistry --role OPERATOR_ROLE --account WALLET --network mumbai
+cat $WALLETS_FILE | xargs -n1 -IWALLET npx hardhat grant-role --contract ShareholderRegistry --role OPERATOR_ROLE --account WALLET --network $NETWORK
 echo "Minting shares"
-cat $WALLETS_FILE | xargs -n1 -IWALLET npx hardhat mint-share WALLET --network mumbai
+cat $WALLETS_FILE | xargs -n1 -IWALLET npx hardhat mint-share WALLET --network $NETWORK
 echo "Setting MANAGING_BOARD status"
-cat $WALLETS_FILE | xargs -n1 -IWALLET npx hardhat set --account WALLET --status managing_board --network mumbai
+cat $WALLETS_FILE | xargs -n1 -IWALLET npx hardhat set --account WALLET --status managing_board --network $NETWORK
 echo "Minting tokens"
-cat $WALLETS_FILE | xargs -n1 -IWALLET npx hardhat mint --account WALLET --amount 42 --network mumbai
+cat $WALLETS_FILE | xargs -n1 -IWALLET npx hardhat mint --account WALLET --amount 42 --network $NETWORK

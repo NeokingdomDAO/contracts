674c26aa8c0d38aee89212625e07ae24331ed99e
vrde <agranzot@mailbox.org>
2022-07-23 10:36:59 +0200

    Add amount to mint-share task (#91)

--- tasks/status.ts
+++ tasks/status.ts
@@ -9,18 +9,21 @@ import { readFileSync } from "fs";
 
 task("mint-share", "Mint a share to an address")
   .addPositionalParam("account", "The address")
-  .setAction(async ({ account }: { account: string }, hre) => {
-    const contract = await loadContract(
-      hre,
-      ShareholderRegistry__factory,
-      "ShareholderRegistry"
-    );
+  .addPositionalParam("amount", "Amount to mint")
+  .setAction(
+    async ({ account, amount }: { account: string; amount: string }, hre) => {
+      const contract = await loadContract(
+        hre,
+        ShareholderRegistry__factory,
+        "ShareholderRegistry"
+      );
 
-    const tx = await contract.mint(account, parseEther("1"));
-    console.log("  Submitted tx", tx.hash);
-    const receipt = await tx.wait();
-    console.log("  Transaction included in block", receipt.blockNumber);
-  });
+      const tx = await contract.mint(account, parseEther(amount));
+      console.log("  Submitted tx", tx.hash);
+      const receipt = await tx.wait();
+      console.log("  Transaction included in block", receipt.blockNumber);
+    }
+  );
 
 task(
   "transfer-shares-batch",

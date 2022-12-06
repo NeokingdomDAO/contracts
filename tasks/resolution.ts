6f249e3b2036edba14018ebd9056bf944ecc91d8
Nicola Miotto <miotto@posteo.de>
2022-12-06 15:58:27 +0100

    add voter-vote task

--- tasks/resolution.ts
+++ tasks/resolution.ts
@@ -16,3 +16,17 @@ task("resolution:execute", "Execute a resolution")
     const receipt = await tx.wait();
     console.log("  Transaction included in block", receipt.blockNumber);
   });
+
+task("voter-vote", "Get voter details for resolutions ID")
+  .addParam("account", "The address")
+  .addParam("id", "Resolution ID")
+  .setAction(async ({ account, id }: { account: string; id: string }, hre) => {
+    const contract = await loadContract(
+      hre,
+      ResolutionManager__factory,
+      "ResolutionManager"
+    );
+
+    const result = await contract.getVoterVote(id, account);
+    console.log(result);
+  });

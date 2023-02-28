7b86570cd791658217fb25b06dcb5725620adcd4
vrde <agranzot@mailbox.org>
2023-02-28 15:56:38 +0100

    Add dev wallets

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,9 +13,6 @@ import("./tasks").catch((e) => console.log("Cannot load tasks", e.toString()));
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
 const BLAST_API_KEY = process.env.BLAST_API_KEY || "";
-const RINKEBY_PRIVATE_KEY =
-  process.env.RINKEBY_PRIVATE_KEY! ||
-  "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
 const MUMBAI_PRIVATE_KEY =
   process.env.RINKEBY_PRIVATE_KEY! ||
   "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
@@ -77,16 +74,9 @@ const config: HardhatUserConfig = {
     localhost: {
       url: "http://127.0.0.1:8545",
     },
-    localhost: {
-      url: "http://127.0.0.1:8545",
-    },
     kovan: {
       url: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [KOVAN_PRIVATE_KEY],
-    },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [RINKEBY_PRIVATE_KEY],
     },
     mumbai: {
       url: `https://polygon-testnet.blastapi.io/${BLAST_API_KEY}`,
@@ -125,13 +115,7 @@ const config: HardhatUserConfig = {
     gasPrice: GAS_PRICE,
     outputFile: GASREPORT_FILE,
     noColors: NO_COLORS,
-    excludeContracts: [
-      "ERC20Mock",
-      "ResolutionManagerV2Mock",
-      "ShareholderRegistryMock",
-      "NeokingdomTokenMock",
-      "VotingMock",
-    ],
+    excludeContracts: ["mocks", "ERC20", "ERC20Upgradeable"],
   },
   typechain: {
     outDir: "./typechain",

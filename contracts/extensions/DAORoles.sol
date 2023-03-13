55a850c69a5813e517cd4040f30f36c9a2feb73d
vrde <agranzot@mailbox.org>
2023-03-13 12:12:24 +0100

    Make DAORoles not upgradeable

--- contracts/extensions/DAORoles.sol
+++ contracts/extensions/DAORoles.sol
@@ -2,14 +2,10 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract DAORoles is AccessControlUpgradeable {
-    function initialize() public initializer {
-        __AccessControl_init();
+contract DAORoles is AccessControl {
+    constructor() {
         _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
-
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() initializer {}
 }

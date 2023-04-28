7b0239b0289b515a0fa979646c8ba52a4dbfe495
Nicola Miotto <miotto@posteo.de>
2023-04-28 15:49:41 +0200

    start resolution is 25

--- contracts/ResolutionManager/ResolutionManagerBase.sol
+++ contracts/ResolutionManager/ResolutionManagerBase.sol
@@ -88,7 +88,6 @@ abstract contract ResolutionManagerBase {
         _shareholderRegistry = shareholderRegistry;
         _governanceToken = governanceToken;
         _voting = voting;
-        _currentResolutionId = 24;
 
         // TODO: check if there are any rounding errors
         _addResolutionType("amendment", 66, 14 days, 6 days, false);
@@ -100,7 +99,7 @@ abstract contract ResolutionManagerBase {
         _addResolutionType("routine", 51, 3 days, 2 days, true);
         _addResolutionType("genesis", 100, 0 days, 4 days, false);
 
-        _currentResolutionId = 1;
+        _currentResolutionId = 25;
     }
 
     modifier onlyPending(uint256 resolutionId) {

259af992bcd3122a173a5ff2a81edaf1ac29e7c6
Nicola Miotto <miotto@posteo.de>
2023-06-20 14:13:26 +0200

    fix init internal market

--- contracts/InternalMarket/InternalMarket.sol
+++ contracts/InternalMarket/InternalMarket.sol
@@ -16,7 +16,7 @@ contract InternalMarket is Initializable, HasRole, InternalMarketBase {
         DAORoles roles,
         IGovernanceToken tokenInternal
     ) public initializer {
-        _initialize(tokenInternal, 5 minutes);
+        _initialize(tokenInternal, 7 days);
         _setRoles(roles);
     }
 

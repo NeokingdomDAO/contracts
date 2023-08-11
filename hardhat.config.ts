c1ba0a81fc103aa3e5888ad74c1853babaec441e
Nicola Miotto <miotto@posteo.de>
2023-08-11 11:08:09 +0200

    same pragma for all contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -44,7 +44,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.19",
+        version: "0.8.16",
         settings: {
           optimizer: {
             enabled: true,
